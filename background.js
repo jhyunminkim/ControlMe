92ad63419566337fdac990d0b69b5436a3fcce31
Victor Kang <Victor Kang>
2016-09-17 19:36:45 -0400

    added flag for blocking and time

--- background.js
+++ background.js
@@ -1,15 +1,17 @@
+var block = false;
+var beginTime = 0;
+
 chrome.webRequest.onBeforeRequest.addListener(
-	function(details) { return {cancel: true}; },
+	function(details) { 
+		if (block == true) {
+			return {cancel: true};
+		} else {
+			return {cancel: false};
+		}
+	},
 	{urls: ["*://*.facebook.com/*",
 			"*://*.reddit.com/*"
 			]
 	},
 	["blocking"]
 );
-
-function TurnOn(){
-   var myButton = document.getElementByID("Button1");
-   document.removeChild(myButton);
-
-}
-
--- popup.js
+++ popup.js
@@ -1,11 +1,18 @@
-window.addEventListener("load", TurnOn);                                                                                           
+window.addEventListener("load", TurnOn);
+                                                                                           
 function TurnOn(){
    var myButton = document.getElementById("id1");
    if(myButton){
-      myButton.addEventListener("click", function(){
-	   document.body.removeChild(myButton);
-      });
+   	  console.log("button exists");
+      myButton.addEventListener("click", handleClick);
    }
 }
 
- 
+function handleClick() {
+	console.log("went into handler");
+  	chrome.runtime.getBackgroundPage(function(bg) {
+	    bg.block = true;
+	    bg.beginTime = new Date().getTime();
+	});
+
+}
